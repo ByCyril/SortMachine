1deb28ede6b6608d59379ae9c81721acf873ede7
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-10-21 18:07:03 -0700

    bubble sort complete

--- main.js
+++ main.js
@@ -1,81 +1,57 @@
-var snakeX = 15;
-var snakeY = 15;
-var height = 30;
-var width = 50;
-var interval = 25;
-var increment = 1;
+const canvasHeight = 505;
+const canvasWidth = 1005;
+const blockWidth = 5;
 
-//game variables
-var length = 0;
-var tailX = [snakeX];
-var tailY = [snakeY];
-var fX;
-var fY;
-var running = false;
-var gameOver = false;
-var direction = -1; // up = 0, down = -1, left = 1, right = 2
-var int;
-var score = 0;
-var distance = 100000;
+var boxHeights = [];
 
-createMap();
-createBlock(1, 15, "green");
-createBlock(2, 10, "pink");
-createBlock(3, 11, "yellow");
-createBlock(4, 1, "blue");
-createBlock(5, 4, "red");
-createBlock(6, 9, "orange");
-createBlock(7, 12, "white");
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  createBoxes();
+  frameRate(25);
+}
 
-function createMap() {
-  document.write("<center><table>");
+function draw() {
+  background(0);
+  bubbleSort();
+}
 
-  for (var y = 0; y < height; y++) {
-    document.write("<tr>");
+i = 0;
 
-    for (var x = 0; x < width; x++) {
-      if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
-        document.write("<td class='wall' id='" + x + "-" + y + "'></td>");
-      } else {
-        document.write("<td class='blank' id='" + x + "-" + y + "'></td>");
+function bubbleSort() {
+  if (i < boxHeights.length) {
+    for (var j = i + 1; j < boxHeights.length; j++) {
+      if (boxHeights[j] < boxHeights[i]) {
+        swapRect(boxHeights, i, j);
       }
     }
-
-    document.write("</tr>");
+  } else {
+    noLoop();
   }
 
-  document.write("</table></center>");
-}
+  i++;
 
-function get(x, y) {
-  return document.getElementById(x + "-" + y);
-}
-
-function set(x, y, value) {
-  if (x != null && y != null) {
-    get(x, y).setAttribute("class", value);
-  }
-}
-
-function rand(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getType(x, y) {
-  return get(x, y).getAttribute("class");
-}
-
-function print(x) {
-  console.log(x);
-}
-
-function createBlock(w, h, color) {
-  console.log(h);
-  h = 29 - h;
-  for (var i = 29; i > h; i--) {
-    console.log(15 - w, i, color);
-    set(15 - w, i, color);
+  for (let i = 0; i < boxHeights.length; i++) {
+    fill(255);
+    rect(
+      i * blockWidth,
+      canvasHeight - (boxHeights[i] + 5),
+      blockWidth,
+      boxHeights[i]
+    );
   }
 }
 
-function swap(x1, x2) {}
+function createBoxes() {
+  for (var i = 0; i < 1000; i = i + blockWidth) {
+    var h = Math.floor(Math.random() * 500 + 1);
+    boxHeights.push(h);
+    rect(i, canvasHeight - (h + 5), blockWidth, h);
+  }
+  console.log("made", boxHeights);
+}
+
+function swapRect(ar, i, j) {
+  let temp = ar[i];
+  ar[i] = ar[j];
+  ar[j] = temp;
+}
